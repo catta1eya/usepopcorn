bdb80700c4357bb62c99cc4388c7d75ffcde2911
catta1eya <p.kubica1605@gmail.com>
2025-01-10 13:25:20 +0100

    Added focusing search input on component mount

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,6 +1,28 @@
-import { useState } from "react";
+import { useEffect, useRef } from "react";
 
 const Search = ({ query, setQuery }) => {
+  const inputEl = useRef(null); //in case of DOM element we usually initialize with null
+
+  useEffect(() => {
+    const callback = (e) => {
+      if (document.activeElement === inputEl.current) return;
+
+      if (e.code === "Enter") {
+        inputEl.current.focus(); //set focus for search input on component mount
+        setQuery("");
+      }
+    };
+    document.addEventListener("keydown", callback);
+
+    return () => document.addEventListener("keydown", callback);
+  }, [setQuery]);
+
+  // Wrong way of selecting DOM elements
+  // useEffect(() => {
+  //   const el = document.querySelector(".search");
+  //   el.focus();
+
+  // }, []);
   return (
     <input
       className="search"
@@ -8,6 +30,7 @@ const Search = ({ query, setQuery }) => {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 };
